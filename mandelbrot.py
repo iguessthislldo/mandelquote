48a80257ff896900cee0108b975737cbe40f7e26
Fred Hornsey <fred@hornsey.us>
2023-12-31 19:43:47 -0600

    Generate image in parallel slices

--- mandelbrot.py
+++ mandelbrot.py
@@ -6,6 +6,7 @@ import random
 import math
 import time
 import traceback
+import concurrent.futures as futures
 
 from PIL import Image, ImageOps, ImageDraw, ImageShow
 
@@ -19,7 +20,7 @@ except ImportError:
 
 class FileOutput:
     def __init__(self):
-        self.n = 0
+        self.n = 1
         self.img = None
         self.resolution = (1000, 600)
 
@@ -40,6 +41,7 @@ class Mandelbrot:
         self.sat_avg = 3
         self.count = 15
         self.min_interval = 0
+        self.slice_height = 1
 
     def mandelbrot(self, x, y):
         c = complex(x, y)
@@ -50,34 +52,62 @@ class Mandelbrot:
             n += 1
         return n
 
-    def generate_mandelbrot(self, draw=None, edges=None):
-        w, h = self.out.resolution
+    def generate_slice(self, row_start, h, get_img=False, get_edges=False):
+        edges = [] if get_edges else None
+        w = self.out.resolution[0]
+        draw = None
+        if get_img:
+            img = Image.new('HSV', (w, h), (0, 0, 0))
+            draw = ImageDraw.Draw(img)
         re_start, re_end, im_start, im_end = self.window
         for col in range(0, w):
-            for row in range(0, h):
+            for row in range(row_start, row_start + h):
                 x = re_start + (col / w) * (re_end - re_start)
-                y = im_start + (row / h) * (im_end - im_start)
+                y = im_start + (row / self.out.resolution[1]) * (im_end - im_start)
                 m = self.mandelbrot(x, y)
                 if draw is not None:
                     if m == self.max_iter:
+                        # White looks better on eInk display
                         hue = 0
                         sat = 0
-                        val = 255
                     else:
+                        # Make color more saturated the closer it is to an edge
                         hue = int(m * 255 / self.max_iter)
                         sat = int((hue + 255 * self.sat_avg) / (1 + self.sat_avg))
-                        val = 255 if m < self.max_iter else 0
-                    draw.point((col, row), (hue, sat, val))
-                if edges is not None and m == (self.max_iter - 1):
+                    val = 255
+                    draw.point((col, row - row_start), (hue, sat, val))
+                if get_edges and m == (self.max_iter - 1):
                     edges.append((x, y))
+        return (row_start, img.convert('RGB') if get_img else None, edges)
 
-    def generate_mandelbrot_image(self, edges=None):
-        img = Image.new('HSV', self.out.resolution, (0, 0, 0))
-        draw = ImageDraw.Draw(img)
-        self.generate_mandelbrot(draw, edges)
-        return img.convert('RGB')
+    def generate(self, get_img=True, edges=None):
+        if get_img:
+            img = Image.new('RGB', self.out.resolution, (0, 0, 0))
+            w, h = self.out.resolution
+        get_edges = edges is not None
+        with futures.ProcessPoolExecutor() as ex:
+            # Break image up into slices that can be computed in parallel
+            jobs = []
+            for i in range(0, h // self.slice_height):
+                row_start = i * self.slice_height
+                jobs.append(ex.submit(self.generate_slice,
+                    row_start, self.slice_height, get_img, get_edges))
+            # If slices don't fit neatly, make an extra one for the remainder
+            rem = h % self.slice_height
+            if rem != 0:
+                jobs.append(ex.submit(self.generate_slice,
+                    row_start + self.slice_height, rem, get_img, get_edges))
 
-    def generate_mandelbrot_image_seq(self):
+            # As jobs finish, copy slices into the result image
+            for job in futures.as_completed(jobs):
+                row_start, slice_img, slice_edges = job.result()
+                if get_edges:
+                    edges.extend(slice_edges)
+                if get_img:
+                    img.paste(slice_img, (0, row_start))
+        return img
+
+    def generate_seq(self):
         for i in range(0, self.count):
             n = i + 1
             print(f'{n}/{self.count}', self.window)
@@ -85,7 +115,7 @@ class Mandelbrot:
             start_gen = time.monotonic()
             last = i == self.count - 1
             edges = None if last else []
-            img = self.generate_mandelbrot_image(edges)
+            img = self.generate(True, edges)
             if edges is not None:
                 print(len(edges), 'edges that can be used')
             end_gen = time.monotonic()
@@ -121,7 +151,7 @@ else:
 print(f'{out.resolution[0]}x{out.resolution[1]}')
 while True:
     try:
-        m.generate_mandelbrot_image_seq()
+        m.generate_seq()
     except Exception:
         if loop:
             traceback.print_exc()

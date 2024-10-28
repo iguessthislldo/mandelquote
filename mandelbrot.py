5325b85f878bca7343a289d2c3a36f8eec359245
Fred Hornsey <fred@hornsey.us>
2024-10-28 01:06:24 -0500

    Add Options, Faster Skips, Many Small Improvments

--- mandelbrot.py
+++ mandelbrot.py
@@ -6,7 +6,7 @@ from time import sleep
 from argparse import ArgumentParser
 import random
 import math
-import time
+from time import time
 import traceback
 import concurrent.futures as futures
 import colorsys
@@ -14,6 +14,7 @@ import textwrap
 import array
 
 from PIL import Image, ImageOps, ImageDraw, ImageShow, ImageFont
+
 
 try:
     from inky.auto import auto as Inky
@@ -57,32 +58,63 @@ class FileOutput:
         self.n += 1
 
 
-class Mandelbrot:
+class Log:
     def __init__(self):
+        self.level = 0
+        self.timers = []
+
+    def print(self, *args):
+        print('    ' * self.level, '-', *args)
+
+    def push_level(self):
+        self.level += 1
+
+    def pop_level(self):
+        self.level -= 1
+
+    def push_time(self, *args):
+        self.timers += [(time(), args)]
+
+    def pop_time(self):
+        start, args = self.timers.pop()
+        interval = time() - start
+        self.print(interval, 's to', *args)
+        return interval
+
+
+class EdgeError(Exception):
+    pass
+
+
+class Mandelbrot:
+    def __init__(self, workers, p=2, smooth=True, sat=True, dynamic_color=True):
+        self.workers = workers
+        self.min_worker_pixels = 100000
         self.window = (-2, 1, -1, 1)
         self.max_iter = 75
         self.zoom = 2
         # Must be <1 or None, smaller means more saturated, None disables desaturation
-        self.sat_weight = 0.0005
-        self.slice_height = 101
-        self.smooth = True
+        self.sat_weight = 0.0005 if sat else None
+        self.smooth = smooth
         # Color range is evaluated separately for each zoom
-        self.dynamic_color = True
-        self.p = 2
+        self.dynamic_color = dynamic_color
+        self.p = p
         self.escape_radius = 10
         self.init_color = (0, 0, 0)
         self.max_iter_color = (255, 255, 255)
 
-    def mandelbrot(self, x, y):
+    def mandelbrot(self, x, y, get_float):
         c = complex(x, y)
         z = 0
         n = 0
         while abs(z) <= self.escape_radius and n < self.max_iter:
             z = z ** self.p + c
             n += 1
-        n_float = n
-        if self.smooth and n < self.max_iter:
-            n_float =- math.log(math.log(abs(z), self.escape_radius), self.p)
+        n_float = None
+        if get_float:
+            n_float = n
+            if n < self.max_iter:
+                n_float =- math.log(math.log(abs(z), self.escape_radius), self.p)
         return n, n_float
 
     def _color(self, min_n, n_float):
@@ -112,55 +144,72 @@ class Mandelbrot:
             c = self._color(min_n, n)
         return c
 
-    def generate_slice(self, resolution, row_start, slice_height, get_data=False, get_edges=False):
+    def mandelbrot_python_impl(self, resolution, row_start, slice_height,
+            get_int_data, get_float_data, get_edges):
         edges = [] if get_edges else None
-        w = resolution[0]
+        w, h = resolution
         int_data = None
         float_data = None
+        get_data = get_int_data or get_float_data
         if get_data:
             init = [0] * (w * slice_height)
-            int_data = array.array('I', init)
-            float_data = array.array('f', init)
+            if get_int_data:
+                int_data = array.array('I', init)
+            if get_float_data:
+                float_data = array.array('d', init)
         re_start, re_end, im_start, im_end = self.window
         for col in range(0, w):
             for row in range(row_start, row_start + slice_height):
                 x = re_start + (col / w) * (re_end - re_start)
-                y = im_start + (row / resolution[1]) * (im_end - im_start)
-                n, n_float = self.mandelbrot(x, y)
+                y = im_start + (row / h) * (im_end - im_start)
+                n, n_float = self.mandelbrot(x, y, get_float_data)
                 if get_data:
                     index = (row - row_start) * w + col
-                    int_data[index] = n
-                    float_data[index] = n_float
+                    if get_int_data:
+                        int_data[index] = n
+                    if get_float_data:
+                        float_data[index] = n_float
                 if get_edges and n == (self.max_iter - 1):
                     edges.append((x, y))
-        return (
-            row_start,
-            slice_height,
-            int_data if get_data else None,
-            float_data if get_data else None,
-            edges,
-        )
+        return (int_data, float_data, edges)
 
-    def generate(self, resolution, get_img=False, edges=None):
+    def generate_slice(self, resolution, row_start, slice_height,
+            get_int_data, get_float_data, get_edges):
+        int_data, float_data, edges = self.mandelbrot_python_impl(
+            resolution, row_start, slice_height,
+            get_int_data, get_float_data, get_edges)
+        return (row_start, slice_height, int_data, float_data, edges)
+
+    def generate(self, log, resolution, get_img=False, edges=None):
         w, h = resolution
         img = None
         if get_img:
             img = Image.new('RGBA', resolution, (0, 0, 0))
         get_edges = edges is not None
 
-        with futures.ProcessPoolExecutor() as ex:
+        log.push_time('generate and join slices')
+        with futures.ThreadPoolExecutor(max_workers = self.workers) as ex:
+            if w * h > self.min_worker_pixels:
+                max_slice_height = max(h // ex._max_workers, 1)
+            else:
+                max_slice_height = h
+
             # Break image up into slices that can be computed in parallel
+            get_int_data = get_img
+            get_float_data = get_img and self.smooth
             jobs = []
-            print('Images is made of', h / self.slice_height, 'slices')
-            for i in range(0, h // self.slice_height):
-                row_start = i * self.slice_height
+            log.print(resolution, 'image is made of', h / max_slice_height, 'slices')
+            for i in range(0, h // max_slice_height):
+                row_start = i * max_slice_height
                 jobs.append(ex.submit(self.generate_slice,
-                    resolution, row_start, self.slice_height, get_img, get_edges))
+                    resolution, row_start, max_slice_height,
+                    get_int_data, get_float_data, get_edges))
             # If slices don't fit neatly, make an extra one for the remainder
-            rem = h % self.slice_height
+            rem = h % max_slice_height
             if rem != 0:
                 jobs.append(ex.submit(self.generate_slice,
-                    resolution, row_start + self.slice_height, rem, get_img, get_edges))
+                    resolution, row_start + max_slice_height, rem,
+                    get_int_data, get_float_data, get_edges))
 
             # Gather slices as jobs finish
             min_n = sys.maxsize if self.dynamic_color else 0
@@ -174,9 +223,17 @@ class Mandelbrot:
                         for n in int_data:
                             min_n = min(n, min_n)
                     slices.append((row_start, slice_height, int_data, float_data))
+        log.pop_time()
+
+        if get_edges:
+            msg = str(len(edges)) + ' edges that can be used'
+            log.print(msg)
+            if not edges:
+                raise EdgeError(msg)
 
         # Color and copy slices into the result image
         if get_img:
+            log.push_time('color')
             draw = ImageDraw.Draw(img)
             for row_start, slice_height, int_data, float_data in slices:
                 for col in range(0, w):
@@ -185,6 +242,7 @@ class Mandelbrot:
                         n = int_data[index]
                         n_float = float_data[index]
                         draw.point((col, row), self.color(min_n, n, n_float))
+            log.pop_time()
 
         return img
 
@@ -195,11 +253,11 @@ class Mandelbrot:
         self.max_iter = int(self.max_iter * 1.1)
 
 
-class QuoteWriter:
-    def __init__(self, width):
-        self.quotes_path = 'quotes.txt'
-        self.margin = 20
+class TextWriter:
+    def __init__(self, width, font_size):
         self.box_margin = 10
+        self.text_margin = 10
+        self.abs_text_margin = self.box_margin + self.text_margin
         self.box_color = '#bbbbbbbb'
         self.font_path = 'monofur.ttf'
         self.font_size = 24
@@ -207,54 +265,100 @@ class QuoteWriter:
         self.stroke_width = 2
         self.stroke_fill = '#ffffff'
 
-        with open(self.quotes_path) as quotes_file:
-            self.quotes = [q.strip().split(' -- ') for q in quotes_file.readlines()]
-
         self.font = ImageFont.truetype(self.font_path, size=self.font_size)
 
+        test_char = '|'
+        line_size = self.font.getbbox(test_char)
+        self.char_height = line_size[3]
+        self.char_width = line_size[2]
         self.col_width = 1
-        line_size = self.font.getbbox('X' * (self.col_width + 1))
-        self.line_height = line_size[3]
-        while line_size[2] < (width - self.margin * 2):
-            line_size = self.font.getbbox('X' * (self.col_width + 1))
+        while line_size[2] <= (width - self.abs_text_margin * 2):
+            line_size = self.font.getbbox(test_char * (self.col_width + 1))
             self.col_width += 1
 
+    def wrap_text(self, *args):
+        return textwrap.wrap(' '.join([str(x) for x in args]), width=self.col_width)
+
     def draw_text(self, draw, offset, text):
-        draw.text((self.margin, offset), text, font=self.font,
+        draw.text((self.abs_text_margin, offset), text, font=self.font,
             fill=self.fill, stroke_width=self.stroke_width, stroke_fill=self.stroke_fill)
-        return offset + self.line_height
+        return offset + self.char_height
 
-    def write(self, img, rng):
-        quote_text, quote_attr = rng.choice(self.quotes)
-        lines = textwrap.wrap(quote_text, width=self.col_width)
-
+    def start_text_box(self, img, line_count, bottom=False, use_text_width=None):
         text_box = Image.new('RGBA', img.size, 0)
         draw = ImageDraw.Draw(text_box)
+        text_height = self.char_height * line_count
+        box_height = text_height + self.text_margin * 2
+        if bottom:
+            box_y = img.height - self.box_margin - box_height
+        else:
+            box_y = self.box_margin
+        if use_text_width is not None:
+            box_w = self.char_width * use_text_width + self.box_margin * 2
+        else:
+            box_w = img.width - self.box_margin * 2
+        box_x = self.box_margin
         draw.rectangle(
-            (
-                self.box_margin, self.box_margin,
-                img.width - self.box_margin, self.box_margin + self.line_height * (len(lines) + 2)
-            ),
+            (box_x, box_y, box_x + box_w, box_y + box_height),
             fill=self.box_color,
         )
+        return text_box, draw, box_y + self.text_margin
 
-        offset = self.margin
-        for line in lines:
-            offset = self.draw_text(draw, offset, line)
-        self.draw_text(draw, offset, '-- ' + quote_attr)
-
+    def end_text_box(self, img, text_box):
         return Image.alpha_composite(img, text_box)
 
 
+class QuoteWriter(TextWriter):
+    def __init__(self, width):
+        super().__init__(width, font_size=24)
+        self.quotes_path = 'quotes.txt'
+        with open(self.quotes_path) as quotes_file:
+            self.quotes = [q.strip().split(' -- ') for q in quotes_file.readlines()]
+
+    def write(self, img, rng):
+        quote_text, quote_attr = rng.choice(self.quotes)
+        lines = self.wrap_text(quote_text)
+        text_box, draw, offset = self.start_text_box(img, len(lines) + 1)
+        for line in lines:
+            offset = self.draw_text(draw, offset, line)
+        self.draw_text(draw, offset, '-- ' + quote_attr)
+        return self.end_text_box(img, text_box)
+
+
+class InfoWriter(TextWriter):
+    def __init__(self, width):
+        super().__init__(width, font_size=12)
+
+    def write(self, img, *args):
+        lines = self.wrap_text(*args)
+        text_box, draw, offset = self.start_text_box(img,
+            len(lines), bottom=True, use_text_width=max([len(line) for line in lines]))
+        for line in lines:
+            offset = self.draw_text(draw, offset, line)
+        return self.end_text_box(img, text_box)
+
+
 class Sequence:
-    def __init__(self, out):
-        self.count = 15
-        self.skip = 0
-        self.min_interval = 0
-        self.seed = random.randrange(sys.maxsize)
+    def __init__(self, resolution, skip_resolution, out, count, skip, min_interval, seed, text,
+            workers, p, smooth, sat, dynamic_color):
+        self.resolution = resolution
+        self.skip_resolution = skip_resolution
+        self.count = skip + count
+        self.skip = skip
+        self.min_interval = min_interval
+        self.seed = random.randrange(sys.maxsize) if seed is None else seed
+        self.text = text
+        self.workers = workers
+        self.p = p
+        self.smooth = smooth
+        self.sat = sat
+        self.dynamic_color = dynamic_color
 
         self.out = out
-        self.quotes = QuoteWriter(out.resolution[0])
+        width = resolution[0]
+        if text:
+            self.quotes = QuoteWriter(width)
+            self.info = InfoWriter(width)
 
     @property
     def seed(self):
@@ -265,58 +369,108 @@ class Sequence:
         self._seed = value
         self.rng = random.Random(self._seed)
 
-    def generate(self):
-        mandelbrot = Mandelbrot()
+    def _generate_and_show(self, log):
+        mandelbrot = Mandelbrot(self.workers, self.p, self.smooth, self.sat, self.dynamic_color)
+        skip_res = list(self.skip_resolution)
         for i in range(0, self.count):
             n = i + 1
             get_img = n > self.skip
-            print(f'{n}/{self.count} get_img: {get_img}', mandelbrot.window)
+            log.print(f'{n}/{self.count} get_img: {get_img}', mandelbrot.window)
+            log.push_time('generate and show')
+            log.push_level()
 
-            start_gen = time.monotonic()
+            log.push_time('generate')
             last = i == self.count - 1
             edges = None if last else []
-            img = mandelbrot.generate(
-                self.out.resolution, get_img=get_img, edges=edges)
-            if edges is not None:
-                print(len(edges), 'edges that can be used')
-            end_gen = time.monotonic()
-            print(end_gen - start_gen, 's to generate')
+            while True:
+                res = self.resolution if get_img else skip_res
+                log.print(res)
+                log.push_level()
+                try:
+                    img = mandelbrot.generate(log, res, get_img=get_img, edges=edges)
+                except EdgeError:
+                    if get_img:
+                        raise
+                    skip_res = [n * 2 for n in skip_res]
+                    res = skip_res
+                    continue
+                finally:
+                    log.pop_level()
+                break
+            log.pop_time()
 
             if img and get_img:
-                img = self.quotes.write(img, self.rng)
+                log.push_time('show')
+                if self.text:
+                    img = self.quotes.write(img, self.rng)
                 self.out.set_image(img)
                 self.out.show()
+                log.pop_time()
 
-                end_show = time.monotonic()
-                print(end_show - end_gen, 's to show')
+            interval = log.pop_time()
+            log.pop_level()
 
-                total = end_show - start_gen
-                if total < self.min_interval:
-                    sleep(self.min_interval - total)
+            if interval < self.min_interval:
+                sleep_for = self.min_interval - interval
+                log.print('Sleeping for', sleep_for, 's')
+                sleep(sleep_for)
 
             if not last:
                 mandelbrot.zoom_in(self.rng.choice(edges))
 
+    def generate_and_show(self, loop):
+        while True:
+            log = Log()
+            try:
+                self._generate_and_show(log)
+            except Exception:
+                if loop:
+                    traceback.print_exc()
+                else:
+                    raise
+            if not loop:
+                break
 
-if file_output:
-    loop = False
-    seq = Sequence(FileOutput())
-    seq.skip = 1
-else:
-    loop = True
-    seq = Sequence(Inky(ask_user=True, verbose=True))
-    seq.min_interval = 60 * 10
-    seq.skip = 2
 
-print(f'{seq.out.resolution[0]}x{seq.out.resolution[1]}')
-print(f'seed: {seq.seed}')
-while True:
-    try:
-        seq.generate()
-    except Exception:
-        if loop:
-            traceback.print_exc()
-        else:
-            raise
-    if not loop:
-        break
+def parse_size(s):
+    return [int(n) for n in s.split('x')]
+
+
+if __name__ == '__main__':
+    arg_parser = ArgumentParser()
+    arg_parser.add_argument('--size', type=parse_size)
+    arg_parser.add_argument('--skip-size', type=parse_size)
+    arg_parser.add_argument('--count', '-c', type=int, default=15)
+    arg_parser.add_argument('--skip', '-s', type=int)
+    arg_parser.add_argument('--min-interval', '-i', type=int)
+    arg_parser.add_argument('--seed', type=int)
+    arg_parser.add_argument('--no-text', action='store_false', dest='text')
+    arg_parser.add_argument('--workers', '-j', type=int, default=None)
+    arg_parser.add_argument('-p', type=int, default=2)
+    arg_parser.add_argument('--no-smooth', action='store_false', dest='smooth')
+    arg_parser.add_argument('--no-sat', action='store_false', dest='sat')
+    arg_parser.add_argument('--no-dynamic-color', action='store_false', dest='dynamic_color')
+    args = arg_parser.parse_args()
+
+    if file_output:
+        out = FileOutput()
+        skip_default = 1
+        min_interval_default = 0
+        loop = False
+    else:
+        out = Inky(ask_user=True, verbose=True)
+        skip_default = 2
+        min_interval_default = 60 * 10
+        loop = True
+    resolution = args.size or out.resolution
+    skip_resolution = args.skip_size or resolution
+
+    skip = skip_default if args.skip is None else args.skip
+    min_interval = min_interval_default if args.min_interval is None else args.min_interval
+    seq = Sequence(resolution, skip_resolution, out, args.count, skip, min_interval, args.seed,
+        args.text, args.workers, args.p, args.smooth, args.sat, args.dynamic_color)
+
+    print(f'{seq.resolution[0]}x{seq.resolution[1]}')
+    print(f'seed: {seq.seed}')
+
+    seq.generate_and_show(loop)
